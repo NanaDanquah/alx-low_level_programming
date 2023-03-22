c61dfcf55edf27602d2f8623403b06432b9d9b5c
nanaDanquah <danquahbadugabriel@gmail.com>
2023-03-21 22:15:33 -0700

    fixing no description found in main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
-
-
-	int main(void)
-	{
+/**
+ * main - prints _putchar and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,6 +14,6 @@
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
-	}
 
+	return (0);
+}
