a89937f80e953079168058f41401f8e7d9a0f7e4
nanaDanquah <danquahbadugabriel@gmail.com>
2023-03-21 22:52:52 -0700

    fixed all betty style issues

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercae
+ *
+ */
 
-
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
-
-	void print_alphabet(void)
+void print_alphabet(void)
 {
 		char letter;
 
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,27 @@
-#include "main.h"
-
-
-	/**
-	 * main - check the code
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
+#include <stdio.h>
+/**
+ * print_to_98 - Prints all natural numbers
+ * from n to 98.
+ *
+ * @n: input number.
+ *
+ * Return: no return.
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
 	{
-	    print_to_98(0);
-	    print_to_98(98);
-	    print_to_98(111);
-	    print_to_98(81);
-	    print_to_98(-10);
-	    return (0);
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
 	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
+}
